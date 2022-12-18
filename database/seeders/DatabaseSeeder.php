d1d84c8299f578162501c56c1f00bd6bb6ff2a1b
xarthaa <manager.arthachan@gmail.com>
2022-12-18 16:46:28 +0800

    seeder fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(1)->create();
+        $this->call([
+            UnhcrSeeder::class,
+        ]);
     }
 }
