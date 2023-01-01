461574adee06bd66d118870e21c64ee3791e3bc8
xarthaa <manager.arthachan@gmail.com>
2023-01-02 01:06:20 +0800

    success trait bug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\API\Traits\SuccessResponseTrait;
+use App\Http\Controllers\Api\Traits\SuccessResponseTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
