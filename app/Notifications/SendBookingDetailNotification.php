d1f25153e5fa0a55480238b26e23bbf946756d52
arthaputra <manager.arthachan@gmail.com>
2023-02-23 20:32:10 +0800

    add queue

--- app/Notifications/SendBookingDetailNotification.php
+++ app/Notifications/SendBookingDetailNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SendBookingDetailNotification extends Notification
+class SendBookingDetailNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
